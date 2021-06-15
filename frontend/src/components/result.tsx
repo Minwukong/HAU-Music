83e2ea148f83bedb0dfd9560f60923432bcac9db
Minwukong <jh02035@gmail.com>
2021-06-15 19:02:23 +0900

    페이지 설정
    페이지 설정 컴포넌트 연결

---
+++ frontend/src/components/result.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const result = () => {
+    return (
+        <div>
+            <h4>result 페이지</h4>
+        </div>
+    );
+};
+
+export default result;
+
---
+++ frontend/src/components/root.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const root = () => {
+    return (
+        <div>
+            <h4>root 페이지</h4>
+        </div>
+    );
+};
+
+export default root;
