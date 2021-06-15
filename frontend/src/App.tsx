e9e656bd2e9e4f1aa962ce950d8906df8116ceb4
Minwukong <jh02035@gmail.com>
2021-06-15 20:03:55 +0900

    material ui style 적용
    front module 추가 -----------
    
    material ui style 적용

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import root from './components/root'
-import test from './components/test'
-import result from './components/result'
+import Root from './components/root'
+import Test from './components/test'
+import Result from './components/result'
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path='/' component={root} exact />
-        <Route path='/test' component={test} exact />
-        <Route path='/result' component={result} exact />
+        <Route path='/' component={Root} exact />
+        <Route path='/test' component={Test} exact />
+        <Route path='/result' component={Result} exact />
       </Switch>
     </Router>
   );
