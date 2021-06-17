b0506878b5b08b58ff5dc8c849a120332a15da09
Minwukong <jh02035@gmail.com>
2021-06-17 19:27:17 +0900

    2021/06/17
    ----------프론트 모듈 추가----
    
    material ui 다크 모드, 기본 설정
    
    오디오 라이브러리

---
+++ frontend/src/components/result_Button.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { makeStyles } from "@material-ui/core/styles";
+import Button from '@material-ui/core/Button'; // Button을 import 한다.
+
+
+const useStyles = makeStyles({
+    root: {
+      background: 'linear-gradient(45deg, #FE6B8B 30%, #F333 90%)',
+      border: 0,
+      borderRadius: 3,
+      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      color: 'white',
+      height: 48,
+      padding: '0 30px',
+    },
+  });
+
+
+
+const Result1 = () => {
+    const classes = useStyles();
+    return (
+
+        <div>
+            <Button className={classes.root} >
+                Hello World
+            </Button>
+
+        </div>
+       
+    );
+};
+
+export default Result1;
+
